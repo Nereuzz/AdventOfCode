17260155d6d90f470c3ea153bcc796af320daaa8
Peter Thomsen <thomsenpeter1995@hotmail.com>
2023-12-14 21:43:37 +0100

    day 14

--- aoc2023_F#/14/14.fsx
+++ aoc2023_F#/14/14.fsx
@@ -24,21 +24,22 @@ let ParseMap map =
     
 
 let input =
-    File.ReadAllLines "aoc2023_F#/14/test.txt"
+    File.ReadAllLines "14/input.txt"
     |> Array.map (fun x -> Seq.toList x)
     |> List.ofArray
     |> ParseMap
-    |> List.transpose
 
-let rec DoneRolling row tmp =
+let rec DoneRolling row =
     match row with
     | [] -> true
-    | x::xs -> if x = R
-               then DoneRolling xs true
-               else false
+    | R::R::xs -> DoneRolling (xs)
+    | R::E::xs -> DoneRolling (E::xs)
+    | E::R::_ -> false
+    | E::E::xs -> DoneRolling (E::xs)
+    | E::SR::xs -> DoneRolling (SR::xs)
+    | _::xs -> DoneRolling xs
 
 let rec MoveRocksLeft row =
-    printfn "Rolling..%A" row
     let rec TiltLeft row =
      match row with
      | [] -> []
@@ -48,9 +49,66 @@ let rec MoveRocksLeft row =
                      else x::TiltLeft (xs::xss)
 
     match TiltLeft row with
-    | xs -> if DoneRolling xs true
-                           then xs
-                           else printfn "Rerolling..%A" xs; MoveRocksLeft xs
-                           
-input
-List.map MoveRocksLeft input
+    | xs -> if DoneRolling xs
+            then xs
+            else MoveRocksLeft xs
+
+let ComputeLoad totalRows row col i =
+    match i with
+    | R -> totalRows - row
+    | _ -> 0
+
+let day14_1 = 
+    let map = input |> List.transpose |> List.map MoveRocksLeft |> List.transpose |> array2D
+    let ComputedLoadForMap = ComputeLoad (Array2D.length2 map)
+    Array2D.mapi ComputedLoadForMap map
+    |> Seq.cast<int> 
+    |> Seq.sum
+
+day14_1 |> printfn "day 14 part 1: %d"
+
+let cycle input =
+ input
+ // Move north
+|> List.transpose
+|> List.map MoveRocksLeft
+|> List.transpose
+// Move west
+|> List.map MoveRocksLeft
+// Move South
+|> List.rev
+|> List.transpose
+|> List.map MoveRocksLeft
+|> List.transpose
+|> List.rev
+// Move East
+|> List.map List.rev
+|> List.map MoveRocksLeft
+|> List.map List.rev
+
+let rec cycleMap map times =
+    match times with
+    | x when x > 0 -> cycleMap (cycle map) (times-1)
+    | _ -> map
+  
+
+let map = input |> array2D
+let ComputedLoadForMap = ComputeLoad (Array2D.length2 map)
+
+let rec day14_2 map states cycles actualCycles= 
+    match cycles with
+    | x when x > 0 -> let newMap = cycle map
+                      if Map.containsKey newMap states
+                      then let size = (Map.find newMap states) - cycles
+                           let start = Map.find newMap states
+                           let initCycleCount = Map.values states |> Seq.cast |> Seq.max
+                           let step = (actualCycles - (initCycleCount-start+1)) % size
+                           cycleMap newMap step
+                           |> array2D 
+                           |> Array2D.mapi ComputedLoadForMap
+                           |> Seq.cast<int> 
+                           |> Seq.sum 
+                      else day14_2 newMap (Map.add newMap cycles states) (cycles-1) actualCycles
+    | _ -> failwith "Damn, no cycle.."
+
+day14_2 input Map.empty 1000 1000000000 |> printfn "Day 14 part 2: %A"
