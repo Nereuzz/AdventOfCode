75b3786e511c00032778e1dfd9a82b8f0a8d97f9
Peter Thomsen <thomsenpeter1995@hotmail.com>
2023-12-20 19:40:36 +0100

    day 20 part 1

--- aoc2023_F#/20/20.fsx
+++ aoc2023_F#/20/20.fsx
@@ -1,5 +1,6 @@
 open System.IO
 
+
 type Pulse =
     | Low
     | High
@@ -43,7 +44,7 @@ let CreateModuleMap wireModule =
 
 let input =
     let tmp =
-     File.ReadAllLines "20/test.txt"
+     File.ReadAllLines "20/input.txt"
      |> Seq.map (fun x -> x.Split("->") |> List.ofArray)
      |> List.ofSeq
      |> List.map (List.map (fun (y:string) -> y.Replace(" ", "")))
@@ -53,28 +54,53 @@ let input =
     let conses = Map.filter (fun k v -> match v.Type with |Conjunction _-> true |_ -> false) tmp |> Map.values |> Seq.cast<Module> |> List.ofSeq
     AddConjunctionsEntrypoints tmp conses
 
-let queue = [("button","broadcaster",Low)]
-
 let rec AddSignalsToQueue queue source destinations pulse =
     match destinations with
     | [] -> queue
     | x::xs -> AddSignalsToQueue (queue @ [(source, x, pulse)]) source xs pulse
 
+let PushButton queue =
+    AddSignalsToQueue queue "button" ["broadcaster"] Low
+
 let rec ResolveQueue (map:Map<string,Module>) result queue =
     match queue with
-    | [] -> result
+    | [] -> (map, result)
     | x::xs -> let (s,d,p) = x
+               if d = "rx"
+               then ResolveQueue map result xs
+               else
                let m = Map.find d map
                match m.Type,p with
                | FlipFlop state, Low -> let map' = Map.add d {m with Type=FlipFlop (not state)} map
                                         let pulse = if state then Low else High
-                                        let queue' = AddSignalsToQueue queue d m.Destination pulse
+                                        let queue' = AddSignalsToQueue xs d m.Destination pulse
                                         let result' =
-                                            if state
-                                            then {result with Lows=result.Lows+(List.length m.Destination)}
-                                            else {result with Highs=result.Highs+(List.length m.Destination)}
+                                          if state
+                                          then {result with Lows=result.Lows+(List.length m.Destination)}
+                                          else {result with Highs=result.Highs+(List.length m.Destination)}
                                         ResolveQueue map' result' queue'
-               | FlipFlop state, High -> ResolveQueue map result queue
-               | Conjunction cmap -> let cmap' = Map.Add s p cmap
-                                     let map' = Map.add d {m with Type=Conjunction cmap'} map
+               | FlipFlop _, High -> ResolveQueue map result xs
+
+               | Conjunction cmap, _ -> let cmap' = Map.add s p cmap
+                                        let map' = Map.add d {m with Type=Conjunction cmap'} map
+                                        let memory = cmap'.Values |> Seq.cast<Pulse> |> List.ofSeq
+                                        let result',queue' = 
+                                         if  List.forall (fun x -> match x with High -> true | Low -> false) memory
+                                         then {result with Lows = result.Lows+List.length m.Destination},AddSignalsToQueue xs d m.Destination Low
+                                         else {result with Highs = result.Highs+List.length m.Destination},AddSignalsToQueue xs d m.Destination High
+                                        ResolveQueue map' result' queue'
+               | Broadcaster, _ -> let queue' = AddSignalsToQueue xs d m.Destination Low
+                                   let result' = {result with Lows=result.Lows+List.length m.Destination}
+                                   ResolveQueue map result' queue'                      
                                      
+let rec PushButtonNTimes map result times =
+    
+    match times with
+    | 0 -> result
+    | _ -> let (map', result') = ResolveQueue map result (PushButton [])
+           let result'' = {result' with Lows=result'.Lows+1}
+           PushButtonNTimes map' result'' (times - 1)
+    
+
+let hmm = PushButtonNTimes input {Highs=0; Lows=0} 1000
+printfn "day 20 part 1: %d" (hmm.Highs * hmm.Lows)
