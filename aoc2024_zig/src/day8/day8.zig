424cb927310b5d1226f497f7349bfcb0fd1a9b75
Nereuzzz <thomsenpeter1995@hotmail.com>
2024-12-08 19:46:07 +0100

    day8 part 2

--- aoc2024_zig/src/day8/day8.zig
+++ aoc2024_zig/src/day8/day8.zig
@@ -4,7 +4,7 @@ const input = @embedFile("input.txt");
 
 pub fn main() !void {
     print("Day 8:\n", .{});
-    const sol1 = try p1();
+    const sol1 = try p1(false);
     const sol2 = try p2();
 
     print("Part1: {}\nPart2: {}\n", .{ sol1, sol2 });
@@ -13,7 +13,7 @@ pub fn main() !void {
 const Antenna = struct { x: u64, y: u64 };
 const Antinode = struct { x: i64, y: i64 };
 
-fn p1() !u64 {
+fn p1(part2: bool) !u64 {
     const allocator = std.heap.page_allocator;
     const antennaListType = std.BoundedArray(Antenna, 10000);
     var map = std.AutoArrayHashMap(u8, antennaListType).init(allocator);
@@ -54,22 +54,58 @@ fn p1() !u64 {
                     const distX = try std.math.sub(i64, @intCast(antenna.x), @intCast(nextAntenna.x));
                     const distY = try std.math.sub(i64, @intCast(antenna.y), @intCast(nextAntenna.y));
                     const possibilites = [2]Antinode{ .{ .x = try std.math.add(i64, @intCast(antenna.x), distX), .y = try std.math.add(i64, @intCast(antenna.y), distY) }, .{ .x = try std.math.sub(i64, @intCast(nextAntenna.x), distX), .y = try std.math.sub(i64, @intCast(nextAntenna.y), distY) } };
-                    for (possibilites) |p| {
-                        // print("Checking possibillity: {}\n", .{p});
-                        if (p.x >= 0 and p.x < rowIdx and p.y >= 0 and p.y < colIdxx) {
+                    if (!part2) {
+                        for (possibilites) |p| {
+                            // print("Checking possibillity: {}\n", .{p});
+                            if (p.x >= 0 and p.x < rowIdx and p.y >= 0 and p.y < colIdxx) {
+                                var exists = false;
+                                for (resultArray.slice()) |knownNode| {
+                                    exists = knownNode.x == p.x and knownNode.y == p.y;
+                                    if (exists) {
+                                        // print("Skipping inserting known {}\n", .{knownNode});
+                                        break;
+                                    }
+                                }
+                                if (!exists) {
+                                    // print("Adding {} to results\n", .{p});
+                                    try resultArray.insert(0, p);
+                                    // print("Result after add: {}\n", resultArray.slice());
+                                }
+                            }
+                        }
+                    } else {
+                        var x: i64 = @intCast(antenna.x);
+                        var y: i64 = @intCast(antenna.y);
+                        while (x >= 0 and x < rowIdx and y >= 0 and y < colIdxx) {
                             var exists = false;
                             for (resultArray.slice()) |knownNode| {
-                                exists = knownNode.x == p.x and knownNode.y == p.y;
+                                exists = knownNode.x == x and knownNode.y == y;
                                 if (exists) {
-                                    // print("Skipping inserting known {}\n", .{knownNode});
                                     break;
                                 }
                             }
                             if (!exists) {
-                                // print("Adding {} to results\n", .{p});
-                                try resultArray.insert(0, p);
-                                // print("Result after add: {}\n", resultArray.slice());
+                                try resultArray.insert(0, .{ .x = x, .y = y });
                             }
+                            x = try std.math.add(i64, x, distX);
+                            y = try std.math.add(i64, y, distY);
+                        }
+
+                        x = @intCast(nextAntenna.x);
+                        y = @intCast(nextAntenna.y);
+                        while (x >= 0 and x < rowIdx and y >= 0 and y < colIdxx) {
+                            var exists = false;
+                            for (resultArray.slice()) |knownNode| {
+                                exists = knownNode.x == x and knownNode.y == y;
+                                if (exists) {
+                                    break;
+                                }
+                            }
+                            if (!exists) {
+                                try resultArray.insert(0, .{ .x = x, .y = y });
+                            }
+                            x = try std.math.sub(i64, x, distX);
+                            y = try std.math.sub(i64, y, distY);
                         }
                     }
                 }
@@ -81,5 +117,5 @@ fn p1() !u64 {
 }
 
 fn p2() !u64 {
-    return 0;
+    return try p1(true);
 }
