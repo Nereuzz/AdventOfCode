d767423276f14423edbac1607d1989b30a4056fa
Peter Thomsen <thomsenpeter1995@hotmail.com>
2024-12-08 12:17:43 +0100

    move to desktop

--- aoc2024_zig/src/day8/day8.zig
+++ aoc2024_zig/src/day8/day8.zig
@@ -10,8 +10,21 @@ pub fn main() !void {
     print("Part 1: {}\nPart2:{}\n", .{ sol1, sol2 });
 }
 
+const Antenna = struct { x: u64, y: u54 };
+
 fn p1() !u64 {
-    return 0;
+    const allocator = std.heap.page_allocator;
+    var map = std.AutoHashMap(u8, Antenna).init(allocator);
+    defer map.deinit();
+    
+    var iter = std.mem.tokenizeAny(u8, input, "\n");
+    while (iter.next()) |line| {
+        for (line, 0..) |char, idx| {
+            if (std.ascii.isAlphanumeric(char)) {
+                if (map.cont)
+}
+}
+}
 }
 
 fn p2() !u64 {
