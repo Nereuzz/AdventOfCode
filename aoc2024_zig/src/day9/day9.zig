4c582ab7b7376487d3c94fbf4703bbcfc4ec38d4
Nereuzzz <thomsenpeter1995@hotmail.com>
2024-12-09 13:49:24 +0100

    damn, Ellen woke up during code cleaning

--- aoc2024_zig/src/day9/day9.zig
+++ aoc2024_zig/src/day9/day9.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
 const print = std.debug.print;
-const input = @embedFile("input.txt");
+const input = @embedFile("test.txt");
 const Array = @import("/Users/peter/Code/ZigArrayLib/src/root.zig");
+const emptyBlock = std.math.maxInt(u64);
 
 pub fn main() !void {
     const sol1 = try p1();
@@ -11,6 +12,45 @@ pub fn main() !void {
 }
 
 fn p1() !u64 {
+    var diskArray = try parseInput();
+    var diskSlice: []u64 = diskArray.slice();
+    var blockPointer: u64 = 0;
+    while (diskSlice[blockPointer] != emptyBlock) : (blockPointer += 1) {
+        continue;
+    }
+    var filePointer: u64 = @intCast(diskArray.slice().len - 1);
+    // print("Sanity: {any}\n", .{diskSlice[filePointer - 5 .. filePointer + 1]});
+    while (blockPointer < filePointer) {
+        diskSlice[blockPointer] = diskSlice[filePointer];
+        diskSlice[filePointer] = emptyBlock;
+
+        while (diskSlice[filePointer] == emptyBlock) : (filePointer -= 1) {
+            continue;
+        }
+
+        while (diskSlice[blockPointer] != emptyBlock) : (blockPointer += 1) {
+            continue;
+        }
+    }
+
+    var result: u64 = 0;
+    var sumIdx: u64 = 0;
+    while (sumIdx < diskSlice.len) : (sumIdx += 1) {
+        if (diskSlice[sumIdx] != emptyBlock) {
+            result += diskSlice[sumIdx] * sumIdx;
+        }
+    }
+
+    return result;
+}
+
+fn p2() !u64 {
+    const diskArray = try parseInput();
+    _ = diskArray;
+    return 0;
+}
+
+fn parseInput() !std.BoundedArray(u64, 100000) {
     var diskIter = std.mem.window(u8, input, 1, 1);
 
     var fileId: u64 = 0;
@@ -31,45 +71,12 @@ fn p1() !u64 {
             fileId += 1;
         } else {
             while (blockLength > 0) : (blockLength -= 1) {
-                try diskArray.insert(diskIdx, std.math.maxInt(u64));
+                try diskArray.insert(diskIdx, emptyBlock);
                 diskIdx += 1;
             }
             file = true;
         }
     }
 
-    var diskSlice: []u64 = diskArray.slice();
-    var blockPointer: u64 = 0;
-    while (diskSlice[blockPointer] != std.math.maxInt(u64)) : (blockPointer += 1) {
-        continue;
-    }
-    var filePointer: u64 = @intCast(diskArray.slice().len - 1);
-    // print("Sanity: {any}\n", .{diskSlice[filePointer - 5 .. filePointer + 1]});
-    while (blockPointer < filePointer) {
-        diskSlice[blockPointer] = diskSlice[filePointer];
-        diskSlice[filePointer] = std.math.maxInt(u64);
-
-        while (diskSlice[filePointer] == std.math.maxInt(u64)) : (filePointer -= 1) {
-            continue;
-        }
-
-        while (diskSlice[blockPointer] != std.math.maxInt(u64)) : (blockPointer += 1) {
-            continue;
-        }
-    }
-
-    var result: u64 = 0;
-    var sumIdx: u64 = 0;
-    while (sumIdx < diskSlice.len) : (sumIdx += 1) {
-        if (diskSlice[sumIdx] != std.math.maxInt(u64)) {
-            result += diskSlice[sumIdx] * sumIdx;
-        }
-    }
-    print("Last mul: {} * {}\n", .{ diskSlice[sumIdx - 1], sumIdx - 1 });
-
-    return result;
-}
-
-fn p2() !u64 {
-    return 0;
+    return diskArray;
 }
