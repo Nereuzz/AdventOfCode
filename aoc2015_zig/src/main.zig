221c9da67f738320accf559fbd7c46f6e8daec6c
Peter Thomsen <thomsenpeter1995@hotmail.com>
2024-12-10 19:21:59 +0100

    update main

--- aoc2015_zig/src/main.zig
+++ aoc2015_zig/src/main.zig
@@ -1,6 +1,10 @@
 const std = @import("std");
 const day1 = @import("day1/day1.zig");
+const day2 = @import("day2/day2.zig");
+const day4 = @import("day4/day4.zig");
 
 pub fn main() !void {
     try day1.main();
+    try day2.main();
+    try day4.main();
 }
