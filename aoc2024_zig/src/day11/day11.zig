e63a066e60494a224f6739eb3772d7f51735f35e
Peter Thomsen <pt@dalux.com>
2024-12-12 10:28:47 +0100

    part2 - Håbløst langsomt

--- aoc2024_zig/src/day11/day11.zig
+++ aoc2024_zig/src/day11/day11.zig
@@ -1,9 +1,9 @@
 const std = @import("std");
 const print = std.debug.print;
-const input = @embedFile("test.txt");
+const input = @embedFile("input.txt");
 
 pub fn main() !void {
-    const sol1 = try p1();
+    const sol1 = 0; //try p1();
     const sol2 = try p2();
 
     print("Day 11:\nPart 1: {}\nPart 2: {}\n", .{ sol1, sol2 });
@@ -21,8 +21,8 @@ fn p1() !u64 {
         try stones.append(buf);
     }
 
-    const blinks: u64 = 6;
-    for (0..blinks) |i| {
+    const blinkss = 1;
+    for (0..blinkss) |i| {
         _ = i;
         var stoneIdx: u64 = 0;
         const hmm = stones.items;
@@ -69,10 +69,8 @@ fn p1() !u64 {
 const mapType = std.StringHashMap(u64);
 fn p2() !u64 {
     print("----- Part2 -----\n", .{});
-    const allocator = std.heap.page_allocator;
-
-    var map = mapType.init(allocator);
-    defer map.deinit();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
 
     var stones = std.ArrayList([]u8).init(allocator);
     defer stones.deinit();
@@ -84,13 +82,13 @@ fn p2() !u64 {
         try stones.append(buf);
     }
 
+    var map = mapType.init(allocator);
     var result: u64 = 0;
     for (stones.items) |stone| {
-        print("Checking stone {s}\n", .{stone});
-        const tmp = try solveStone(stone, 6, &map, allocator);
-        print("Solved {}\n", .{tmp});
+        const tmp = try solveStone(stone, 75, &map, allocator);
         result += tmp;
     }
+    map.deinit();
     return result;
 }
 
@@ -99,7 +97,9 @@ fn solveStone(stone: []const u8, blinks: comptime_int, map: *mapType, allocator:
         return 1;
     }
 
-    if (map.get(stone)) |result| {
+    const keyBuf = try allocator.alloc(u8, 20);
+    const key = try std.fmt.bufPrint(keyBuf, "{}:{s}", .{ blinks, stone });
+    if (map.*.get(key)) |result| {
         return result;
     }
 
@@ -109,6 +109,8 @@ fn solveStone(stone: []const u8, blinks: comptime_int, map: *mapType, allocator:
     } else if (rule2(stone)) {
         const left = try allocator.dupe(u8, stone[0 .. stone.len / 2]);
         result = try solveStone(left, blinks - 1, map, allocator);
+        allocator.free(left);
+
         const right = try allocator.dupe(u8, stone[stone.len / 2 ..]);
         const parsedRight = std.fmt.parseInt(u64, right, 10) catch 1;
         if (parsedRight == 0) {
@@ -123,17 +125,19 @@ fn solveStone(stone: []const u8, blinks: comptime_int, map: *mapType, allocator:
                 break;
             }
             const komnuu = try allocator.dupe(u8, right[idx..]);
+            defer allocator.free(komnuu);
             result += try solveStone(komnuu, blinks - 1, map, allocator);
         }
+        allocator.free(right);
     } else {
         const value = try std.fmt.parseInt(u64, stone, 10);
-        var buf: [100]u8 = undefined;
-        const newValue = try std.fmt.bufPrint(&buf, "{}", .{value * 2024});
+        // var buf: [30]u8 = undefined;
+        const newValue = try std.fmt.allocPrint(allocator, "{}", .{value * 2024});
         result = try solveStone(newValue, blinks - 1, map, allocator);
     }
 
-    print("Putting now: {s} --- result: {}\n", .{ stone, result });
-    try map.put(stone, result);
+    // print("Putting now: {s} --- result: {}\n", .{ stone, result });
+    try map.*.put(key, result);
     return result;
 }
 
