5920c860bfc9827a22a36697b6366138a04b405e
Peter Thomsen <thomsenpeter1995@hotmail.com>
2024-12-16 19:36:21 +0100

    update gitignore

--- aoc2024_zig/src/day14/day14.zig
+++ aoc2024_zig/src/day14/day14.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const regex = @import("mvzr.zig");
 const print = std.debug.print;
-const input = @embedFile("test.txt");
+const input = @embedFile("input.txt");
 
 const Robot = struct {
     x: i64,
@@ -10,9 +10,9 @@ const Robot = struct {
     vy: i64,
 
     fn lessThan(_: void, a: Robot, b: Robot) bool {
-        if (a.x == b.x)
-            return a.y < b.y;
-        return a.x < b.x;
+        if (a.y == b.y)
+            return a.x < b.x;
+        return a.y < b.y;
     }
 };
 
@@ -36,25 +36,63 @@ pub fn main() !void {
     }
 
     const sol1 = try p1(robots.items, 101, 103, 100);
-    const sol2 = try p2(robots.items, 11, 7, allocator);
+    const sol2 = try p2(robots.items, 101, 103, allocator);
 
     print("Day 14:\nPart 1: {}\nPart 2: {}\n", .{ sol1, sol2 });
 }
 
 fn p2(robs: []Robot, width: i64, height: i64, allocator: std.mem.Allocator) !u64 {
     _ = allocator;
-    var seconds: i64 = 1;
-    for (robs, 0..) |rob, idx| {
-        robs[idx].x = @mod(rob.x + seconds * rob.vx, width);
-        robs[idx].y = @mod(rob.y + seconds * rob.vy, height);
+    const max: u64 = @intCast(width * height);
+    for (101..max) |seconds| { // First 100 iterations done by part 1
+        for (robs, 0..) |rob, idx| {
+            robs[idx].x = @mod(rob.x + rob.vx, width);
+            robs[idx].y = @mod(rob.y + rob.vy, height);
+        }
+
+        std.mem.sort(Robot, robs, {}, Robot.lessThan);
+
+        for (robs, 0..) |_, idx| {
+            if (idx > 500 - 12 or !(robs[idx].y == robs[idx + 1].y and
+                robs[idx].y == robs[idx + 2].y and
+                robs[idx].y == robs[idx + 3].y and
+                robs[idx].y == robs[idx + 4].y and
+                robs[idx].y == robs[idx + 5].y and
+                robs[idx].x + 1 == robs[idx + 1].x and
+                robs[idx].x + 2 == robs[idx + 2].x and
+                robs[idx].x + 3 == robs[idx + 3].x and
+                robs[idx].x + 4 == robs[idx + 4].x and
+                robs[idx].x + 5 == robs[idx + 5].x and
+                robs[idx].x + 6 == robs[idx + 6].x))
+            {
+                continue;
+            } else {
+                // for (0..103) |row| { // Beautiful print <3
+                //     for (0..101) |col| {
+                //         var robStart: usize = 0;
+                //         var robPrinted = false;
+                //         for (robs[robStart..], 0..) |rob, idxp| {
+                //             if (rob.y > row) break;
+                //             if (rob.x == col and rob.y == row and !robPrinted) {
+                //                 print("R", .{});
+                //                 robPrinted = true;
+                //                 robStart = idxp;
+                //                 continue;
+                //             }
+                //         }
+                //         if (robPrinted) {
+                //             robPrinted = false;
+                //             continue;
+                //         }
+                //         print(".", .{});
+                //     }
+                //     print("\n", .{});
+                // } // End of beautiful print
+                return seconds;
+            }
+        }
     }
-
-    std.mem.sort(Robot, robs, {}, Robot.lessThan);
-    for (robs) |rob| print("{any}\n", .{rob});
-
-    seconds += 1;
-
-    return @intCast(seconds);
+    return 99999;
 }
 
 fn p1(robs: []Robot, width: i64, height: i64, seconds: u8) !u64 {
