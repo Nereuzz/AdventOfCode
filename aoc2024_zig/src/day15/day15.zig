ed9f0ed579d0ce63f3bef0c2c8a592121c1e25ac
Nereuzzz <thomsenpeter1995@hotmail.com>
2024-12-17 19:28:57 +0100

    noget spaghetti

--- aoc2024_zig/src/day15/day15.zig
+++ aoc2024_zig/src/day15/day15.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 const print = std.debug.print;
-const input = @embedFile("input.txt");
+const input = @embedFile("test.txt");
+
+const Point = struct { row: u64, col: u64 };
+const Direction = enum { Up, Right, Down, Left };
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -8,9 +11,12 @@ pub fn main() !void {
     const allocator = gpa.allocator();
 
     var map = std.ArrayList([]u8).init(allocator);
+    var map2 = std.ArrayList([]u8).init(allocator);
     defer {
         for (map.items) |row| allocator.free(row);
+        for (map2.items) |row| allocator.free(row);
         map.clearAndFree();
+        map2.clearAndFree();
     }
 
     var inputIter = std.mem.splitSequence(u8, input, "\n\n");
@@ -20,14 +26,63 @@ pub fn main() !void {
     while (mapIter.next()) |line| {
         // print("{s}\n", .{line});
         const row = try allocator.dupe(u8, line);
+        const row2 = try allocator.dupe(u8, line);
         try map.append(row);
+        try map2.append(row2);
     }
 
     var moves = std.mem.tokenizeScalar(u8, inputIter.next().?, '\n');
 
     const sol1 = try p1(map.items, moves.rest(), allocator);
+    const sol2 = try p2(map2.items, moves.rest(), allocator);
 
-    print("Day 15:\nPart 1: {}\n", .{sol1});
+    print("Day 15:\nPart 1: {}\nPart 2: {}\n", .{ sol1, sol2 });
+}
+
+fn p2(inGrid: [][]u8, moves: []const u8, allocator: std.mem.Allocator) !u64 {
+    for (inGrid) |row| print("{s}\n", .{row});
+    var bigGrid = std.ArrayList([]u8).init(allocator);
+    defer {
+        for (bigGrid.items) |row| allocator.free(row);
+        bigGrid.clearAndFree();
+    }
+
+    try expandGrid(inGrid, &bigGrid, allocator);
+    const grid = bigGrid.items;
+
+    var curPos: Point = GetStartPosition(grid);
+
+    for (moves) |move| {
+        if (move == '\n') continue;
+
+        for (grid) |row| print("{s}\n", .{row});
+        const nextPos: Point = GetPosition(move, curPos, 1);
+        const next = grid[nextPos.row][nextPos.col];
+
+        print("{c} --- {},{} --- {c}\n", .{ move, curPos.row, curPos.col, next });
+
+        switch (next) {
+            '#' => continue,
+            '.' => {
+                grid[curPos.row][curPos.col] = '.';
+                grid[nextPos.row][nextPos.col] = '@';
+                curPos = nextPos;
+            },
+            ']' => {
+                if (gotSpace(grid, nextPos, move, 1)) |boxes| {
+                    MoveBox(grid, move);
+                    grid[curPos.row][curPos.col] = '.';
+                    grid[nextPos.row][nextPos.col] = '@';
+                    curPos = nextPos;
+                } else continue;
+            },
+            else => {
+                unreachable;
+            },
+        }
+    }
+
+    return 0;
 }
 
 fn p1(grid: [][]u8, moves: []const u8, allocator: std.mem.Allocator) !u64 {
@@ -38,30 +93,31 @@ fn p1(grid: [][]u8, moves: []const u8, allocator: std.mem.Allocator) !u64 {
         if (move == '\n') continue;
         const nextPos: Point = GetPosition(move, curPos, 1);
         const next = grid[nextPos.row][nextPos.col];
-        // print("{c} -- {c}\n", .{ move, next });
-        if (next == '.') {
-            grid[curPos.row][curPos.col] = '.';
-            grid[nextPos.row][nextPos.col] = '@';
-            curPos = nextPos;
-        }
-        if (next == '#') {
-            continue;
-        }
-        if (next == 'O') {
-            if (gotSpace(grid, nextPos, move, 1)) |stones| {
-                var stoneIter: u64 = stones;
-                // print("stoneIter: {}\n", .{stoneIter});
-                while (stoneIter > 0) {
-                    const stonePos = GetPosition(move, curPos, stoneIter + 1);
-                    // print("{any}\n", .{stonePos});
-                    grid[stonePos.row][stonePos.col] = 'O';
-                    stoneIter -= 1;
-                }
+
+        switch (next) {
+            '.' => {
                 grid[curPos.row][curPos.col] = '.';
                 grid[nextPos.row][nextPos.col] = '@';
                 curPos = nextPos;
-            }
-        } else continue;
+            },
+            '#' => continue,
+            'O' => {
+                if (gotSpace(grid, nextPos, move, 1)) |stones| {
+                    var stoneIter: u64 = stones;
+                    // print("stoneIter: {}\n", .{stoneIter});
+                    while (stoneIter > 0) {
+                        const stonePos = GetPosition(move, curPos, stoneIter + 1);
+                        // print("{any}\n", .{stonePos});
+                        grid[stonePos.row][stonePos.col] = 'O';
+                        stoneIter -= 1;
+                    }
+                    grid[curPos.row][curPos.col] = '.';
+                    grid[nextPos.row][nextPos.col] = '@';
+                    curPos = nextPos;
+                }
+            },
+            else => continue,
+        }
     }
 
     var result: u64 = 0;
@@ -78,12 +134,23 @@ fn p1(grid: [][]u8, moves: []const u8, allocator: std.mem.Allocator) !u64 {
 }
 
 fn gotSpace(grid: [][]u8, pos: Point, direction: u8, spaces: u64) ?u64 {
+    // const currentBoxPiece = grid[pos.row][pos.col];
     const nextPos = GetPosition(direction, pos, 1);
     const next = grid[nextPos.row][nextPos.col];
-    if (next == '.') return spaces;
-    if (next == 'O') return gotSpace(grid, nextPos, direction, spaces + 1);
-    if (next == '#') return null;
-    unreachable;
+    print("next in gotSpace: {c}\n", .{next});
+
+    switch (next) {
+        '.' => return spaces,
+        'O' => return gotSpace(grid, nextPos, direction, spaces + 1),
+        '#' => return null,
+        '[' => switch (direction) {
+            '^' => return gotSpace(grid, nextPos, direction, spaces + 3),
+            'v' => return gotSpace(grid, nextPos, direction, spaces + 3),
+            else => return gotSpace(grid, nextPos, direction, spaces + 1),
+        },
+        ']' => return gotSpace(grid, nextPos, direction, spaces + 1),
+        else => unreachable,
+    }
 }
 
 fn GetPosition(direction: u8, curPos: Point, n: usize) Point {
@@ -105,5 +172,33 @@ fn GetStartPosition(grid: [][]u8) Point {
     unreachable;
 }
 
-const Point = struct { row: u64, col: u64 };
-const Direction = enum { Up, Right, Down, Left };
+fn expandGrid(grid: [][]u8, bigGrid: *std.ArrayList([]u8), allocator: std.mem.Allocator) !void {
+    for (grid, 0..) |row, rowIdx| {
+        var bigRow = std.ArrayList(u8).init(allocator);
+        for (row, 0..) |col, colIdx| {
+            _ = rowIdx;
+            _ = colIdx;
+
+            switch (col) {
+                '#' => {
+                    try bigRow.append(col);
+                    try bigRow.append(col);
+                },
+                'O' => {
+                    try bigRow.append('[');
+                    try bigRow.append(']');
+                },
+                '.' => {
+                    try bigRow.append('.');
+                    try bigRow.append('.');
+                },
+                '@' => {
+                    try bigRow.append('@');
+                    try bigRow.append('.');
+                },
+                else => unreachable,
+            }
+        }
+        try bigGrid.append(bigRow.items);
+    }
+}
