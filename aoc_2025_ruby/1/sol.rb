8e1f2edbf0b603c5338375f3e72a038cf9454eac
Peter Thomsen <thomsenpeter1995@hotmail.com>
2025-12-01 19:51:11 +0100

    day 1 part 2

--- aoc_2025_ruby/1/sol.rb
+++ aoc_2025_ruby/1/sol.rb
@@ -4,7 +4,7 @@ def solve1(start, input)
   input.each do |line|
     direction = line[0]
     steps = line[1..].to_i
-    n = move(n, direction, steps)
+    n = move(n, direction, steps) % 100
     if (n == 0)
       result += 1
     end
@@ -12,9 +12,50 @@ def solve1(start, input)
   result
 end
 
+def solve2(start, input)
+  result = 0
+  n = start
+  input.each do |line|
+    direction = line[0]
+    steps = line[1..].to_i
+
+    dialUnmapped = move(n, direction, steps)
+    wraps = (dialUnmapped / 100).abs
+
+    if n == 0 and direction == 'L'
+      wraps -= 1
+    end
+
+    if dialUnmapped % 100 == 0 and direction == 'R'
+      wraps -= 1
+    end
+
+    result += wraps
+
+    n = dialUnmapped % 100
+    if n == 0
+      result += 1
+    end
+  end
+  result
+end
+
 def move(n, direction, steps)
-  direction == 'L' ? (n - steps) % 100 : (n + steps) % 100
+  direction == 'L' ? (n - steps) : (n + steps)
 end
 
 input = File.read("test.txt").split("\n")
 puts "Part 1: #{solve1(50, input)}"
+puts "debug: #{solve2(50, ["L50"]) == 1}"
+puts "debug: #{solve2(50, ["L150"]) == 2}"
+puts "debug: #{solve2(50, ["L151"]) == 2}"
+puts "debug: #{solve2(0, ["L1"]) == 0}"
+puts "debug: #{solve2(0, ["L100"]) == 1}"
+puts "debug: #{solve2(0, ["L101"]) == 1}"
+puts "debug: #{solve2(0, ["R1"]) == 0}"
+puts "debug: #{solve2(0, ["R100"]) == 1}"
+puts "debug: #{solve2(0, ["R101"]) == 1}"
+puts "debug: #{solve2(50, ["R50"]) == 1}"
+puts "debug: #{solve2(50, ["R150"]) == 2}"
+puts "debug: #{solve2(50, ["R151"]) == 2}"
+#puts "Part 2: #{solve2(50, input)}"
