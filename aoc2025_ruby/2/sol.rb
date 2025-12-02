4645c9dd8ae300b576c912c8cbee5d7ef558d283
Peter Thomsen <pt@dalux.com>
2025-12-02 12:17:22 +0100

    2025 Day 2 part 1

--- aoc2025_ruby/2/sol.rb
+++ aoc2025_ruby/2/sol.rb
@@ -1,5 +1,3 @@
-
-
 def solve1(ranges)
   result = []
   ranges.each do |range|
@@ -13,6 +11,41 @@ def solve1(ranges)
   end
   result.sum()
 end
+
+def solve2(ranges)
+  result = []
+  ranges.each do |range|
+    dashIdx = range.index('-')
+    lower = range[0..dashIdx -1].to_i
+    higher = range[dashIdx + 1..].to_i
+
+    (lower..higher).each do |num|
+      result.append(num) if isRepetetiveSubSequence?(num.to_s)
+    end
+  end
+  result.sum()
+end
+
+def isRepetetiveSubSequence?(str)
+  return str[0] == str[1] if str.length == 2
+  return str.chars.uniq.size == 1 if str.length == 3
+
+  mid = str.length / 2
+  idx = 0
+  while (idx <= mid)
+    seq = str[0..idx]
+    return false if seq.length > mid
+
+    while (seq == str[idx + 1..idx + seq.length])
+      idx += seq.length
+      return true if idx >= str.length - 1
+    end
+    idx += 1
+  end
+
+  return false
+end
+
 
 
 def isMirrorNumber?(str)
@@ -29,3 +62,4 @@ end
 
 ranges = File.read("input.txt").split(',')
 puts "Part 1: #{solve1(ranges)}"
+puts "Part 2: #{solve2(ranges)}"
