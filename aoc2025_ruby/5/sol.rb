e2fc82118b572636249451fe489d4dc7704bc19b
Peter Thomsen <pt@dalux.com>
2025-12-05 15:21:12 +0100

    2025 day 5 part 2

--- aoc2025_ruby/5/sol.rb
+++ aoc2025_ruby/5/sol.rb
@@ -1,4 +1,3 @@
-
 def solve1(input)
   result = []
   ranges = input[0].split("\n")
@@ -9,8 +8,7 @@ def solve1(input)
     low = tmp[0].to_i
     high = tmp[1].to_i
 
-    ingredients.sort().each do |i|
-      i = i.to_i
+    ingredients.map { |i| i.to_i }.sort().each do |i|
       if (i >= low and i <= high)
         ingredients.delete(i.to_s)
         result.append(i)
@@ -21,8 +19,36 @@ def solve1(input)
   result.length
 end
 
+def solve2(input)
+  ranges = (input.split("\n").map {|c| c.split("-")}).map {|t| [t[0].to_i, t[1].to_i]}.sort()
+  resultingRanges = []
+
+  while (ranges.length > 0)
+    oldRanges = resultingRanges
+    low = ranges[0][0]
+    high = ranges[0][1]
+    haha = findRange(ranges, [low,high])
+    ranges = haha[0]
+    resultingRanges.append(haha[1])
+  end
+
+  resultingRanges.map {|r| r[1] - r[0] + 1}.sum()
+end
+
+def findRange(ranges, result)
+  if ranges[1].nil?
+    result[1] = [result[1], ranges[0][1]].max if overlaps(result, ranges[0])
+    return [], result
+  end
+
+  if result[0] <= ranges[1][1] and result[1] >= ranges[1][0]
+    result[1] = [result[1], ranges[1][1]].max
+    return findRange(ranges[1..], result)
+  end
+
+  return ranges[1..], result
+end
+
 input = File.read("input.txt").split("\n\n")
 puts "Part 1: #{solve1(input)}"
-
-
-# 510 too low - off by one without sorting?
+puts "Part 2: #{solve2(input[0])}"
